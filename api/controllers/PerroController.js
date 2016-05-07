61d8745a226093f089b20bb43dd15c4232ee4aa9
Angelo Calvo Alfaro <you@example.com>
2016-05-07 19:47:54 -0300

    permisos

---
+++ api/controllers/PerroController.js
@@ -0,0 +1,21 @@
+/**
+ * PerroController
+ *
+ * @description :: Server-side logic for managing perroes
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+
+
+  /**
+   * `PerroController.nombre()`
+   */
+  nombre: function (req, res) {
+    return res.json({
+      todo: 'nombre() is not implemented yet!'
+    });
+  }
+};
+
