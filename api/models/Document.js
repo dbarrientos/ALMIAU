502b9343f122c92c248ea037282339f023c28a00
David Barrientos <david.barrientos.f@gmail.com>
2016-05-07 19:34:40 -0300

    modelos generados

---
+++ api/models/Document.js
@@ -0,0 +1,17 @@
+/**
+ * Document.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    file : { type: 'string' },
+
+    prize_x_prize : { type: 'string' }
+  }
+};
+
